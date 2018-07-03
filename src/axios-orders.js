87432a927355931379583a195621cae3b1d2bc31
Prakash <prakashdas1998.pd@gmail.com>
2018-07-03 17:01:50 +0530

    add

---
+++ src/axios-orders.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const instance = axis.create({
+    baseURL: "https://burgerbuilder-b802c.firebaseio.com/"
+});
+
+export default instance;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,7 +2,7 @@ import React, { Component} from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
-
+import axios from '../../axios-orders';
 const INGREDIENT_PRICES= {
     salad: 40,
     cheese:50,
@@ -32,6 +32,21 @@ class BurgerBuilder extends Component{
         const oldprice = this.state.totalprice;
         const newprice = oldprice+priceAddition;
         this.setState({totalprice:newprice,ingredients:updated});
+    }
+
+    purchaseHandler = () =>{
+        const order = {
+            ingredients = this.state.ingredients,
+            price: this.state.totalprice,
+            customer:{
+                name:"prakash",
+                address:{
+                    street:"Test street 1",
+                    zip:
+                }
+            }
+        }
+        axios.post('/orders.json');
     }
     render(){
         return (
