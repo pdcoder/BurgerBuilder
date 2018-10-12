81e729693fb9c0891f2d36bf6046724d5ae41681
Prakash <prakashdas1998.pd@gmail.com>
2018-10-13 01:52:47 +0530

    Finished app

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://react-burger.firebaseio.com/'
+    baseURL: 'https://burgerbuilder-b802c.firebaseio.com/'
 });
 
 export default instance;
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -48,9 +48,9 @@ export const auth = (email, password, isSignup) => {
             password: password,
             returnSecureToken: true
         };
-        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyB5cHT6x62tTe-g27vBDIqWcwQWBSj3uiY';
+        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCdx6d_ndNcGInmM6kNswWnx8ALAK_vyv0';
         if (!isSignup) {
-            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyB5cHT6x62tTe-g27vBDIqWcwQWBSj3uiY';
+            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCdx6d_ndNcGInmM6kNswWnx8ALAK_vyv0';
         }
         axios.post(url, authData)
             .then(response => {
